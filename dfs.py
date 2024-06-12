dae14fd44decd5bb3e52dec8b65404f6a253da27
aeh33 <153486449+aeh33@users.noreply.github.com>
2024-06-12 09:32:45 +0900

    update

---
+++ dfs.py
@@ -0,0 +1,13 @@
+def expand(self, depth):
+    result = []
+    if depth > 5: return result 
+    i = self.board.index(0) 
+    if not i in [6, 7, 8]: 
+            result.append(self.get_new_board(i, i+3, moves))
+    if not i in [2, 5, 8]: 
+        result.append(self.get_new_board(i, i+1, moves))
+    if not i in [0, 1, 2] : 
+        result.append(self.get_new_board(i, i-3, moves))
+    if not i in [0, 3, 6] :
+        result.append(self.get_new_board(i, i-1, moves))
+    return result
